51bc4b440b718100f5ab0c56001fcd651e14eac2
Mo Hraiba <hraibam@gmail.com>
2026-02-23 23:26:07 +0100

    nvim config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,38 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = { "lua", "xml", "http", "json", "graphql", "c", "c_sharp","rust" },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup()
+    treesitter.install { 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml', 'c_sharp', 'rust' }
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml', 'cs', 'ru' },
+      callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- folds, provided by Neovim (I don't like folds)
+        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        -- vim.wo.foldmethod = 'expr'
+        -- indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end
 }
+-- return {
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = ":TSUpdate",
+--     config = function()
+--       local config = require("nvim-treesitter.configs")
+--       config.setup({
+--         ensure_installed = { "lua", "xml", "http", "json", "graphql", "c", "c_sharp","rust" },
+--         auto_install = true,
+--         highlight = { enable = true },
+--         indent = { enable = true },
+--       })
+--     end
+--   }
+-- }
