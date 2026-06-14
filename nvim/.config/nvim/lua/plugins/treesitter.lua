e79c1ee4120fd599035568ef77f2d8bec6ba88fb
Mo Hraiba <hraibam@gmail.com>
2026-06-14 13:59:06 +0200

    neovim config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,37 +3,100 @@ return {
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    local treesitter = require("nvim-treesitter")
-    treesitter.setup()
-
-    treesitter.ensure_installed = { "lua", "xml", "http", "json", "graphql", "c", "c_sharp","rust" }
-    treesitter.install { 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml', 'c_sharp', 'rust', 'http' }
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml', 'cs', 'ru' },
-      callback = function()
-        -- syntax highlighting, provided by Neovim
-        vim.treesitter.start()
-        -- folds, provided by Neovim (I don't like folds)
-        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        -- vim.wo.foldmethod = 'expr'
-        -- indentation, provided by nvim-treesitter
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
+    require("nvim-treesitter").setup({
+      ensure_installed = {
+        "lua",
+        "xml",
+        "http",
+        "json",
+        "graphql",
+        "c",
+        "c_sharp",
+        "rust",
+        "java",
+        "vim",
+        "vimdoc",
+        "query",
+        "elixir",
+        "heex",
+        "javascript",
+        "typescript",
+        "html",
+        "yaml",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
     })
-  end
+  end,
 }
 -- return {
---   {
---     "nvim-treesitter/nvim-treesitter",
---     build = ":TSUpdate",
---     config = function()
---       local config = require("nvim-treesitter.configs")
---       config.setup({
---         auto_install = true,
---         highlight = { enable = true },
---         indent = { enable = true },
---       })
---     end
---   }
+--   "nvim-treesitter/nvim-treesitter",
+--   lazy = false,
+--   build = ":TSUpdate",
+--   config = function()
+--     local treesitter = require("nvim-treesitter")
+--     treesitter.setup()
+--
+--     treesitter.ensure_installed = { "lua", "xml", "http", "json", "graphql", "c", "c_sharp", "rust" }
+--     treesitter.install({
+--       "java",
+--       "c",
+--       "lua",
+--       "vim",
+--       "vimdoc",
+--       "query",
+--       "elixir",
+--       "heex",
+--       "javascript",
+--       "typescript",
+--       "html",
+--       "yaml",
+--       "c_sharp",
+--       "rust",
+--       "http",
+--     })
+--
+--     vim.api.nvim_create_autocmd("FileType", {
+--       pattern = {
+--         "java",
+--         "c",
+--         "lua",
+--         "vim",
+--         "vimdoc",
+--         "query",
+--         "elixir",
+--         "heex",
+--         "javascript",
+--         "typescript",
+--         "html",
+--         "yaml",
+--         "cs",
+--         "ru",
+--       },
+--       callback = function()
+--         -- syntax highlighting, provided by Neovim
+--         vim.treesitter.start()
+--         -- folds, provided by Neovim (I don't like folds)
+--         -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+--         -- vim.wo.foldmethod = 'expr'
+--         -- indentation, provided by nvim-treesitter
+--         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+--       end,
+--     })
+--   end,
 -- }
+-- -- return {
+-- --   {
+-- --     "nvim-treesitter/nvim-treesitter",
+-- --     build = ":TSUpdate",
+-- --     config = function()
+-- --       local config = require("nvim-treesitter.configs")
+-- --       config.setup({
+-- --         auto_install = true,
+-- --         highlight = { enable = true },
+-- --         indent = { enable = true },
+-- --       })
+-- --     end
+-- --   }
+-- -- }
