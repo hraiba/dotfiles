21fb9a61c38ca73ed34034736e06911c92c04d55
Mohammad Hraiba <hraibam@gmail.com>
2026-09-07 00:03:26 +0200

    everforst config

--- nvim/.config/nvim/lua/plugins/everforest.lua
+++ nvim/.config/nvim/lua/plugins/everforest.lua
@@ -1,13 +1,15 @@
 return {
-  "neanias/everforest-nvim",
-  version = false,
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  -- Optional; default configuration will be used if setup isn't called.
-  config = function()
-    require("everforest").setup({
-      -- Your config here
-    })
-   vim.cmd[[colorscheme everforest]]
-  end,
-}
+      'sainnhe/everforest',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.everforest_enable_italic = true
+        vim.g.everforest_background = 'hard'
+        vim.g.everforest_transparent_background = 1
+        vim.g.everforest_float_style= 'dim'
+        vim.g.everforest_inlay_hints_background = 'dimmed'
+        vim.cmd[[colorscheme everforest]]
+      end
+    }
